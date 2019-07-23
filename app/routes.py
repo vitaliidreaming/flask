4c2db3f33c50ac85c3ad8f197247ccedd4596ad0
Vitalii <>
2019-07-23 22:56:33 +0300

    another step

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
-import time
+from flask_login import current_user, login_user
+from app.models import User
 
 
 @app.route('/')
@@ -13,8 +14,14 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authencticated:
+        return redirect(url_for['index'])
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
-    return render_template('login.html', titile='Sign In', form=form)
+    return render_template('login.html', title='Sign In', form=form)
