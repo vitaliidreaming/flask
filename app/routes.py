394be3a0528d64537fc829171afb0cd7fd72d488
Vitalii <>
2019-08-02 11:23:10 +0300

    personal page

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,7 @@ from werkzeug.urls import url_parse
 from app import db
 from app.forms import RegistrationForm
 
+
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -53,3 +54,14 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
