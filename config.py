57a7dc3b08fd4ff67bd8e48cedee6852b831043b
Vitalii <>
2019-08-12 13:01:49 +0300

    chapter 7

--- config.py
+++ config.py
@@ -13,4 +13,4 @@ class Config:
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    ADMINS = os.environ.get("ADMINS")
+    ADMINS = [os.environ.get("ADMINS")]
