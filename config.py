f9322ecc2401fe723bf30bae8dbf02181dc05ecc
Vitalii <>
2019-09-17 23:12:08 +0300

    pagination

--- config.py
+++ config.py
@@ -14,3 +14,4 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     ADMINS = [os.environ.get("ADMINS")]
+    POSTS_PER_PAGE = 25
